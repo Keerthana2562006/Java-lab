c68f187665481a0f3727bfae40c8e4b868f5a19a
Keerthana2562006 <keerthanar2566@gmail.com>
2025-03-17 15:17:18 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,22 @@
-import java.sql.SQLOutput;
-
+import java.util.List;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        int a=20;
-        String b="Bhuvan";
-        float c=0.25f;
-        boolean d=true;
-        System.out.println(b);
-        int e=30;
-        int f=a+e;
-        System.out.println(f);
-    }
-}
-//short cut for System.out.println()= sout
+        List<String> list=new ArrayList<>();
+        list.add("apple");
+        list.add("orange");
+        list.add("graps");
+        list.add("kiwi");
+        list.add("banana");
 
+        System.out.println("FRUITS");
+        System.out.println("************");
+        System.out.println(list);
+
+        for(String fruit:list){
+            System.out.println(fruit);
+            }
+
+        }
+
+    }
