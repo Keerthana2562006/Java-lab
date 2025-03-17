5266213f6788fc27a985b541bd33b1fd78d87038
Keerthana2562006 <keerthanar2566@gmail.com>
2025-03-17 15:41:09 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
-import java.util.List;
-import java.util.ArrayList;
+//File Handling in java : Writing a file
+import java.io.FileWriter;
+import java.io.IOException;
+class main{
+    public static void main(String[] args) {
+        try {
+            FileWriter java = new FileWriter("java.txt");  //Creating & Writing a file
+            java.write("Java is a High Level Programming Language");
+            java.close();
+            System.out.println("Java file creation & Writing is Finish");
+        }catch(IOException e){
+            System.out.println("File is not Created : Error");
+            e.printStackTrace();
 
-class Main{
-    public static void main (String[]args) {
-        List<String> fruits = new ArrayList<>();//creating list
-        fruits.add("apple");
-        fruits.add("kiwi");
-        fruits.add("grapes");
-        fruits.add("orange");
-        System.out.println("my fruits : " + fruits);
+
+        }
     }
 }
